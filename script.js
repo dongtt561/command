700f1ec95380b86a51c50a18fb90153c86244ed7
Tran <dongtt@nal.vn>
2016-01-18 07:55:59 +0700

    add image

--- script.js
+++ script.js
@@ -1,53 +1,115 @@
-var COMMAND_MAP = {
-	'air' : 'image/air.png',
-	'earth' : 'earth.png',
-	'fire' : 'fire.png',
-	'water' : 'water.png',
-	'halt' : 'halt.png'
+//config command array
+var COMMAND_ARRAY = {
+    'air' : 'image/air.png',
+    'earth' : 'image/earth.png',
+    'fire' : 'image/fire.png',
+    'water' : 'image/water.png',
+    'halt' : 'image/halt.png'
 };
 
-var NOT_FOUND = 'command not found...';
-var RUNNING = 'is now running...';
-var HALTED = 'is halted...'
+//config command title
+var NOT_FOUND = 'command not found.';
+var RUNNING = 'is now running.';
+var HALTED = 'is halted.';
+
+var IS_NOT_COMMAND = 1;
+var IS_COMMAND = 2;
+var COMMAND_NULL = 3;
 
 $(document).ready(function(){
-	var _o = $('textarea');
-	var enterKeyCode = 13;
-	_o.keypress(function(event){
-		var _keycode = (event.keyCode ? event.keyCode : event.which);
-		if(_keycode == enterKeyCode){
-			_command(_o.val());
-		}
-	});
+    var _o = $('textarea');
+    var enterKeyCode = 13;
+    var i=1;
+    var status = 1;
+    _o.keypress(function(event){
+        var _keycode = (event.keyCode ? event.keyCode : event.which);
+        if(_keycode == enterKeyCode){
+            command = setCommand(_o.val()).toLowerCase();
+            _cmdIsRunning = $('#cmdIsRunning').val();
+            if(validateCommand(command) == IS_COMMAND && command != _cmdIsRunning){
+                if(_cmdIsRunning != '' ) {
+                    setStatusCommnad(_cmdIsRunning,HALTED);
+                }
+                status = RUNNING;
+                $('#cmdIsRunning').val(command);
+                setImage();
+            }
+            
+            if(validateCommand(command) == IS_COMMAND && command == _cmdIsRunning){
+                status = HALTED;
+                $('#cmdIsRunning').val('');
+                setImage();
+            }
+
+            if(validateCommand(command) == IS_NOT_COMMAND){
+                status = NOT_FOUND;
+            }
+
+            if (validateCommand(command) == COMMAND_NULL) {
+                status = COMMAND_NULL;
+            }
+
+            setStatusCommnad(command,status);
+            i++;
+        }
+    });
+
+    $('#reset').click(function(){
+        _o.val('');
+        $('#status').html('');
+        $('#cmdIsRunning').val('');
+        $('#show-image img').removeAttr('src');
+    });
+
 });
 
-var i=1;
-
-function _command(command) {
-	if(validateCommand(command)){
-		if(COMMAND_MAP[command]){
-			$('#status'+i).append('<div id="status' + Number(i+1) + '">' + command.toUpperCase() + '  ' + RUNNING + '</div>')
-		i++;
-		}
-		$('#status').html();
-	}
+function setImage(){
+    var _command = $('#cmdIsRunning').val();
+    var _image = COMMAND_ARRAY[_command];
+    $('#show-image img').removeAttr('src');
+    $('#show-image img').attr('src',_image);
 }
 
-function validateCommand(text){
-	//Mảng chứa các command
-	var _inputArr = text.split('\n');
-	var _len = _inputArr.length;
-
-	//Lấy command vừa gõ
-	var _lastCommand = _inputArr[_len-1];
-
-	//Khai báo Regex
-	var _reg = /[^a-z]/i;
-
-	//Nếu command vừa gõ có kí tự khác các kí tự từ A-Z và a-z thì kết thúc
-	//Ngược lại thực hiện command
-	if(_reg.test(_lastCommand)){
-		return false;
+function setStatusCommnad(command,status){
+	var _class = '';
+	if(status == RUNNING){
+		_class="loading";
 	}
-	return true;
+    if (status != COMMAND_NULL) {
+    	if(status == RUNNING || status == HALTED){
+    		$('.CMDRUNNING').each(function(){
+    			$(this).removeClass('loading');
+    		});
+    	}
+        $('#status').append('<div class="CMDRUNNING ' + _class + '">' + command.toUpperCase() + ' ' + status + '</div>');
+    }
+}
+    
+// Validate command
+function validateCommand(command){
+    //Biểu thức chính quy cho command
+    var _reg = /[^a-z]/i;
+    
+    if (command.trim() == '') {
+        return COMMAND_NULL
+    }
+
+    if(_reg.test(command) || !COMMAND_ARRAY[command]) {
+        return IS_NOT_COMMAND;
+    }
+
+    if (COMMAND_ARRAY[command]) {
+        return IS_COMMAND;
+    }
+}
+
+//Set command
+function setCommand(text){
+    //Mảng chứa các command
+    var _inputArr = text.split('\n');
+    var _len = _inputArr.length;
+
+    //Lấy command vừa gõ
+    var _lastCommand = _inputArr[_len-1];
+    return _lastCommand;
 }
